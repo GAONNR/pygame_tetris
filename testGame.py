be847aa835de3743ad8dd19764a741041907d681
Ji Junseop <gaon0403@gmail.com>
2016-07-24 20:31:21 +0900

    Basic2 branch commit

--- testGame.py
+++ testGame.py
@@ -2,14 +2,23 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
+
+FPS = 30
+clock = pygame.time.Clock()
+
 player = pygame.image.load('player_test.png')
+playerX = 0
 
 while True:
-    screen.fill(0)
-    screen.blit(player, (0, 0))
+    screen.fill((255, 255, 255))
+    screen.blit(player, (playerX, 200))
+
+    playerX += 10
     pygame.display.update()
+    clock.tick(FPS)
 
     for event in pygame.event.get():
         if event.type == QUIT:  ## also pygame.QUIT
