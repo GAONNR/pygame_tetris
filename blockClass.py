97980423553af7872e2618a0c330f4346a287f68
Ji Junseop <gaon0403@gmail.com>
2016-07-30 16:09:58 +0900

    Now blocks can move in X or Y direction, rotate, fall down. Todo: line clearing

--- blockClass.py
+++ blockClass.py
@@ -1,96 +1,57 @@
 import sys, os, math, random
 import pygame
 from pygame.locals import *
+from blockRotation import *
 
 pygame.init()
 
 class Block:
     def __init__(self, color, shape, location):
-        self.image = color
+        self.color = color
         self.location = location
         self.static = False
-        self.leftBlocks = 4
+        self.STATE = 0
+        self.SHAPE = shape
 
-        self.blockArray = [[0, 0, 0, 0],
-                           [0, 0, 0, 0],
-                           [0, 0, 0, 0],
-                           [0, 0, 0, 0]]
+        self.blockArr = [[0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]]
 
-        if shape == 'I':
-            self.blockArray[0][1] = 1    ###   X
-            self.blockArray[1][1] = 1    ###   X
-            self.blockArray[2][1] = 1    ###   X
-            self.blockArray[3][1] = 1    ###   X
-        elif shape == 'J':
-            self.blockArray[0][1] = 1    ###   X
-            self.blockArray[1][1] = 1    ###   X
-            self.blockArray[2][0] = 1    ###  XX
-            self.blockArray[2][1] = 1    ###
-        elif shape == 'L':
-            self.blockArray[0][1] = 1    ###   X
-            self.blockArray[1][1] = 1    ###   X
-            self.blockArray[2][1] = 1    ###   XX
-            self.blockArray[2][2] = 1    ###
-        elif shape == 'O':
-            self.blockArray[0][1] = 1    ###   XX
-            self.blockArray[0][2] = 1    ###   XX
-            self.blockArray[1][1] = 1    ###
-            self.blockArray[1][2] = 1    ###
-        elif shape == 'S':
-            self.blockArray[0][1] = 1    ###   XX
-            self.blockArray[0][2] = 1    ###  XX
-            self.blockArray[1][0] = 1    ###
-            self.blockArray[1][1] = 1    ###
-        elif shape == 'T':
-            self.blockArray[0][0] = 1    ###  XTX
-            self.blockArray[0][1] = 1    ###   X
-            self.blockArray[0][2] = 1    ###
-            self.blockArray[1][1] = 1    ###
-        else: ###     'Z':
-            self.blockArray[0][0] = 1    ###  XX
-            self.blockArray[0][1] = 1    ###   XX
-            self.blockArray[1][1] = 1    ###
-            self.blockArray[1][2] = 1    ###
+        self.blockArr = rotateBlock(shape, self.STATE)
 
-    def show(self, screen, BLOCK_WIDTH):
-        for i in range(4):
-            for j in range(4):
-                if self.blockArray[i][j] > 0:
-                    x, y = self.location
-                    x += j * BLOCK_WIDTH
-                    y += i * BLOCK_WIDTH
-
-                    screen.blit(self.image, (x, y))
-
-    def move(self, screenArray, BLOCK_WIDTH):
+    def move(self, screenArr, BLOCK_WIDTH, DIRECTION):
         if self.static:
-            return False
+            return True
 
         x, y = self.location
-        arrayLocation = (x / BLOCK_WIDTH, y / BLOCK_WIDTH)
+        dx, dy = DIRECTION
 
-        ### remove the block
+        ### remove the block in screenArr
         for i in range(4):
-            if (arrayLocation[1] + i) >= len(screenArray):
+            if (y + i) >= screenArr.getHEIGHT():
                 break
+            if (y + i) < 0:
+                continue
             for j in range(4):
-                x, y = arrayLocation
-                if self.blockArray[i][j] > 0:
-                    screenArray[y + i][x + j] = 0
+                if (x + j) < 0 or (x + j) >= screenArr.getWIDTH():
+                    continue
+                if self.blockArr[i][j] > 0:
+                    screenArr.write(x + j, y + i, -1)
 
         ### check if block is fit for new location. if it doesn't fit, self.static == True
         for i in range(4):
+            if (y + i + dy) < 0:
+                continue
             for j in range(4):
-                x, y = arrayLocation
-
-                if (y + i + 1) >= len(screenArray) or (x + j) >= len(screenArray[0]):
-                    if self.blockArray[i][j] == 0:
+                if (y + i + dy) >= screenArr.getHEIGHT() or (x + j + dx) >= screenArr.getWIDTH() or (x + j + dx) < 0:
+                    if self.blockArr[i][j] == 0:
                         continue
                     else:
                         self.static = True
                         break
 
-                if screenArray[y + i + 1][x + j] > 0 and self.blockArray[i][j] > 0:
+                if screenArr.getXY(x + j + dx, y + i + dy) >= 0 and self.blockArr[i][j] > 0:
                     self.static = True
                     break
 
@@ -99,27 +60,100 @@ class Block:
 
         ### if block fits for new location, update location
         if not self.static:
-            x, y = arrayLocation
-            y += 1
-            arrayLocation = (x, y)
-            self.location = (x * BLOCK_WIDTH, y * BLOCK_WIDTH)
-            print 'loc: ' + str(arrayLocation)
+            x += dx
+            y += dy
+            self.location = (x, y)
+            print 'loc: ' + str(self.location)
 
         ### write new location
         for i in range(4):
-            if (arrayLocation[1] + i) >= len(screenArray):
+            if (y + i) < 0:
+                continue
+            if (y + i) >= screenArr.getHEIGHT():
                 break
             for j in range(4):
-                x, y = arrayLocation
-                screenArray[y + i][x + j] += self.blockArray[i][j]
+
+                if (x + j) < 0 or (x + j) >= screenArr.getWIDTH():
+                    continue
+                if self.blockArr[i][j] > 0:
+                    screenArr.write(x + j, y + i, self.color)
+
+        ### prevent block stopping when there's no place to move in X direction
+        if DIRECTION != (0, 1):
+            self.static = False
+            return False
 
         if self.static:
             return True
         else:
             return False
 
-    def isDead(self):
-        if self.leftBlocks == 0:
-            return True
-        else:
-            return False
+    def moveDown(self, screen, screenArr, BLOCK_WIDTH, GRAVITY):
+        while True:
+            if self.move(screenArr, BLOCK_WIDTH, GRAVITY):
+                return
+
+    def rotate(self, screenArr, BLOCK_WIDTH):
+        tempArr = rotateBlock(self.SHAPE, (self.STATE + 1) % 4)
+
+        x, y = self.location
+
+        for i in range(4):
+            if (y + i) < 0 or (y + i) >= screenArr.getHEIGHT():
+                continue
+            for j in range(4):
+                if (x + j) < 0 or (x + j) >= screenArr.getWIDTH():
+                    continue
+                if self.blockArr[i][j] > 0:
+                    screenArr.write(x + j, y + i, -1)
+
+        for i in range(4):
+            if (y + i) < 0 or (y + i) >= screenArr.getHEIGHT():
+                continue
+            for j in range(4):
+                if (x + j) < 0 or (x + j) >= screenArr.getWIDTH():
+                    continue
+                if tempArr[i][j] > 0 and screenArr.getXY(x + j, y + i) >= 0:
+                    return
+
+        for i in range(4):
+            if (y + i) < 0 or (y + i) >= screenArr.getHEIGHT():
+                continue
+            for j in range(4):
+                if (x + j) < 0 or (x + j) >= screenArr.getWIDTH():
+                    continue
+                if tempArr[i][j] > 0:
+                    screenArr.write(x + j, y + i, color)
+
+        self.blockArr = tempArr
+        self.STATE += 1
+        self.STATE %= 4
+
+
+class Screen:
+    def __init__(self, X, Y):
+        self.arr = [[-1 for col in range(X)] for row in range(Y)]
+        self.WIDTH = X
+        self.HEIGHT = Y
+
+    def write(self, x, y, color):
+        self.arr[y][x] = color
+
+    def getXY(self, x, y):
+        return self.arr[y][x]
+
+    def show(self, screen, BLOCK_WIDTH, images):
+        for i in range(self.HEIGHT):
+            for j in range(self.WIDTH):
+                if self.arr[i][j] < 0:
+                    continue
+
+                y = i * BLOCK_WIDTH
+                x = j * BLOCK_WIDTH
+
+                screen.blit(images[self.arr[i][j]], (x, y))
+
+    def getWIDTH(self):
+        return self.WIDTH
+    def getHEIGHT(self):
+        return self.HEIGHT
--- tetris.py
+++ tetris.py
@@ -13,22 +13,38 @@ SCREEN_SIZE = (360, 680)
 GRID_SIZE_X = 12 ### X GRID 12
 GRID_SIZE_Y = 20 ### Y GRID 20
 
+GRAVITY = (0, 1)
+MOVE_LEFT = (-1, 0)
+MOVE_RIGHT = (1, 0)
+
 LOSE = False
 
 pygame.init()
 
-def handleEvents():
+def handleEvents(block, screen):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
 
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                block.move(screenArr, BLOCK_WIDTH, MOVE_RIGHT)
+            elif event.key == K_LEFT:
+                block.move(screenArr, BLOCK_WIDTH, MOVE_LEFT)
+            elif event.key == K_DOWN:
+                block.moveDown(screen, screenArr, BLOCK_WIDTH, GRAVITY)
+            elif event.key == K_UP:
+                block.rotate(screenArr, BLOCK_WIDTH)
+            else:
+                continue
+
     return True
 
 def loadImages():
-    blockTypes[0] = pygame.image.load('Block1.png')
-    blockTypes[1] = pygame.image.load('Block2.png')
-    blockTypes[2] = pygame.image.load('Block3.png')
-    blockTypes[3] = pygame.image.load('Block4.png')
+    colors[0] = pygame.image.load('Block1.png')
+    colors[1] = pygame.image.load('Block2.png')
+    colors[2] = pygame.image.load('Block3.png')
+    colors[3] = pygame.image.load('Block4.png')
 
 screen = pygame.display.set_mode(SCREEN_SIZE) ### MAKE SCREEN
 clock = pygame.time.Clock()
@@ -47,30 +63,37 @@ background.blit(titleSurface, (20, 20))
 screen.blit(background, (0, 0))
 pygame.display.update()
 
-blockTypes = [0,0,0,0]
+colors = [0,0,0,0]
 loadImages() ### load images of blocks. 0: Blue, 1: Red, 2: Yellow, 3: Green
 
-block1 = Block(blockTypes[0], 'I', (180, 0))
-block2 = Block(blockTypes[1], 'J', (120, 120))
-block3 = Block(blockTypes[2], 'L', (240, 300))
-block4 = Block(blockTypes[3], 'O', (60, 420))
+screenArr = Screen(GRID_SIZE_X, GRID_SIZE_Y)
 
-testBlocks = [block1, block2, block3, block4]
-screenArray = [[0 for col in range(12)] for row in range(20)]
+shapes = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
+FIRSTLOCATION = (5, -4)
+
+randomColor = random.randrange(4)
+currentBlock = Block(randomColor, random.choice(shapes), FIRSTLOCATION)
 
 while not LOSE:
+    if not handleEvents(currentBlock, gameWindow):
+        pygame.quit()
+        exit(0)
+
     gameWindow.fill(BLACK)
 
-    for block in testBlocks:
-        block.show(gameWindow, BLOCK_WIDTH)
-        block.move(screenArray, BLOCK_WIDTH)
+    if currentBlock.move(screenArr, BLOCK_WIDTH, GRAVITY):
+        randomColor = random.randrange(4)
+        currentBlock = Block(randomColor, random.choice(shapes), FIRSTLOCATION)
 
+        if currentBlock.move(screenArr, BLOCK_WIDTH, GRAVITY):
+            LOSE = True
+
+    screenArr.show(gameWindow, BLOCK_WIDTH, colors)
     background.blit(gameWindow, (0, 60))
     screen.blit(background, (0, 0))
 
     pygame.display.update()
     clock.tick(FPS)
 
-    if not handleEvents():
-        pygame.quit()
-        exit(0)
+pygame.quit()
+exit(0)
